d931d784d0e39b82b4966b0d573f6d58ec14d186
VTX <its4vercel@gmail.com>
2025-08-17 17:50:15 -0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,17 +1,11 @@
 -- Loader.lua
 local url = "https://raw.githubusercontent.com/vtxontop/main.lua/refs/heads/main/vtx.lua"
-
 local success, response = pcall(function()
     return game:HttpGet(url)
 end)
 
 if success then
-    local func, loadError = loadstring(response)
-    if func then
-        func()
-    else
-        warn("خطأ في تشغيل السكربت:", loadError)
-    end
+    loadstring(response)()
 else
-    warn("فشل الاتصال بالرابط:", response)
+    warn("فشل تحميل السكربت من الرابط: " .. tostring(response))
 end
