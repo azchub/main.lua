ac5fef730389c0329cbec65afdcaa695748adc26
VTX <its4vercel@gmail.com>
2025-08-18 05:37:54 -0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,5 +1,5 @@
 -- loader.lua
-local url = "https://raw.githubusercontent.com/vtxontop/r3d/main/main.lua"
+local url = "https://raw.githubusercontent.com/vtxontop/r3d/main/vtxx.lua"
 
 local success, response = pcall(function()
     return game:HttpGet(url)
@@ -8,11 +8,11 @@ end)
 if success and response and response ~= "" then
     local func, err = loadstring(response)
     if func then
-        print("✅ تم تحميل main.lua من GitHub")
+        print("✅ تم تحميل vtxx.lua من GitHub")
         func()
     else
         warn("❌ خطأ أثناء تحويل الكود: " .. tostring(err))
     end
 else
-    warn("❌ فشل تحميل الملف من GitHub. تأكد من الرابط أو من وجود main.lua في المستودع r3d.")
+    warn("❌ فشل تحميل الملف من GitHub. تأكد من الرابط أو من وجود vtxx.lua في المستودع r3d.")
 end
