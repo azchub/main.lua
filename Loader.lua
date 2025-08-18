e982ada0d0fa5e26d6a40e73a115303bb0745412
VTX <its4vercel@gmail.com>
2025-08-17 18:16:57 -0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,30 +1,11 @@
--- Loader Script
-local Players = game:GetService("Players")
-local player = Players.LocalPlayer
-local PlayerGui = player:WaitForChild("PlayerGui")
-
--- إنشاء ScreenGui إذا ما كان موجود
-local ScreenGui = PlayerGui:FindFirstChild("VTX_GUI")
-if not ScreenGui then
-    ScreenGui = Instance.new("ScreenGui")
-    ScreenGui.Name = "VTX_GUI"
-    ScreenGui.ResetOnSpawn = false
-    ScreenGui.Parent = PlayerGui
-end
-
--- تحميل سكربت خارجي (vtx.lua)
+-- Loader.lua
+local url = "https://pastebin.com/raw/rY1cTmAU"
 local success, response = pcall(function()
-    return game:HttpGet("https://raw.githubusercontent.com/vtxontop/main.lua/refs/heads/main/vtx.lua")
+    return game:HttpGet(url)
 end)
 
 if success then
-    local func = loadstring(response)
-    if func then
-        func()
-        warn("✅ VTX Script Loaded Successfully")
-    else
-        warn("⚠️ Failed to compile vtx.lua")
-    end
+    loadstring(response)()
 else
-    warn("⚠️ Error loading vtx.lua: ", response)
+    warn("فشل التحميل من Pastebin:", response)
 end
